ffebd583aca7f7f7b9a637c3eb0714f0d30442dc
Youngchan <michael_cho77@naver.com>
2020-06-13 02:02:45 +0900

    Dynamic Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from "react";
 
 //props.fav 또는 {fav}
-function Food({ fav }) {
-  return <h3> I love {fav} </h3>;
+function Food({ name }) {
+  return <h3> I love {name} </h3>;
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!!</h1>
-
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="chichken" />
-      <Food fav="fish" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
   // food component에 fav라는 이름의 property를 kimchi라는 value로 준것이다.
