1258297768970dbb2f07112147ba02ccca319dbf
Youngchan <michael_cho77@naver.com>
2020-06-13 01:16:25 +0900

    Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import Potato from "./Potato";
+
+//props.fav 또는 {fav}
+function Food({ fav }) {
+  return <h3> I love {fav} </h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!</h1>
-      <Potato />
+
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="chichken" />
+      <Food fav="fish" />
     </div>
   );
+  // food component에 fav라는 이름의 property를 kimchi라는 value로 준것이다.
 }
 
 export default App;
