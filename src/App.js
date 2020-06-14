c0b3112d580b9e13dc61b184685552f9730efbee
Youngchan <michael_cho77@naver.com>
2020-06-14 23:14:57 +0900

    Rendering the Movies
      // this.setState({ movies: movies }); 전자는 setStates{}의 movies 후자는 axios data-data의 movies
      // 를 줄여서 위와같이 사용가능 ES6
    
    Movie.js = 실제로 movie를 render함
    
    movie component 는 굳이 state가 필요없기때문에 클래스가아닌 function으로 설계한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Moive";
 
 class App extends React.Component {
   state = {
@@ -7,16 +8,41 @@ class App extends React.Component {
     movies: [],
   };
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false });
   };
+  // this.setState({ movies: movies }); 전자는 setStates{}의 movies 후자는 axios data-data의 movies
+  // 를 줄여서 위와같이 사용가능 ES6
 
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"} </div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
