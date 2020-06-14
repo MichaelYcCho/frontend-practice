da6c996704610b3c74e8ff5cc08a2a7dc3202388
Youngchan <michael_cho77@naver.com>
2020-06-14 16:34:09 +0900

    Component LifeCycle
    https://reactjs.org/docs/react-component.html#mounting
    
      life cycle method react가 component를 생성하고 삭제하는데까지의 사이클을 의미함
    Mounting - update - Unmounting
    (일반적인것은) Page가 바뀌면 component는 죽는것(UnMounting)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
@@ -11,11 +10,20 @@ class App extends React.Component {
   minus = () => {
     this.setState((current) => ({ count: current.count - 1 }));
   };
-
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
   render() {
+    console.log("I'm rendering");
     return (
       <div>
-        <h1>number is: {this.state.count}</h1>
+        <h1>The number is: {this.state.count}</h1>
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
       </div>
