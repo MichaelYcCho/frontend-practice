cd157d8be0988a0ad03eb8e4bb9ae5b4f9434c77
Youngchan <michael_cho77@naver.com>
2020-08-14 01:05:11 +0900

    install antd
    yarn add antd or npm install --save antd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Button } from 'antd';
 import 'App.css';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <button>
+        Hello React
+      </button>
+      <Button type='primary' onClick={() => console.log('clicked')}>
+        Hello React
+      </Button>
+    </div>
+  );
 }
 
 export default App;
