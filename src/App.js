112d831240e19145e324f7b022ac774471c65ca1
Youngchan <michael_cho77@naver.com>
2020-08-14 21:53:06 +0900

    State

--- src/App.js
+++ src/App.js
@@ -2,52 +2,46 @@ import React from 'react';
 import { Button } from 'antd';
 import 'App.css';
 
-class Counter1 extends React.Component {
-  state = {
-    value: this.props.initialValue,
-  }
 
-  onClick = () => {
-    const { value } = this.state;
-    this.setState({ value: value + 1 });
-  };
+const actions = {
+  init(initialValue) {
+    return { value: initialValue }
+
+  },
+  increment(prevState) {
+    return { value: prevState.value + 1 }
+
+  },
+
+  decrement(prevState) {
+
+    return { value: prevState.value - 1 }
+
+  }
+};
+
+
+class Counter1 extends React.Component {
+  state = actions.init(this.props.initialValue)
 
   render() {
     const { value } = this.state;
     return (
       <div>
-        {value}
-        <Button onClick={this.onClick}>+1</Button>
+        Counter1: {value}
+        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
+        <Button onClick={() => this.setState(actions.decrement)}>-1</Button>
       </div>
     );
   }
 
 }
 
-class FruitComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>좋아하는 과일</h1>
-        <ul>
-          {
-            this.props.fruits.map((name, index) => (
-              <li key={index}> {name} </li>
-            ))
-          }
-        </ul>
-      </div>
-    )
-  }
-}
-
 
 function App() {
-  const fruits = ["바나나", "사과", "딸기"];
   return (
     <div>
       <Counter1 initialValue={10} /> {/* 숫자로 표현하고 싶으면 {} 안에서 표기할것 */}
-      <FruitComponent fruits={fruits} />
     </div>
   );
 }
