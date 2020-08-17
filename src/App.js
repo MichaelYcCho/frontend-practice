626ff392485c62f0de8c82688ab90e11403c4331
Youngchan <michael_cho77@naver.com>
2020-08-17 22:55:21 +0900

    Click, Counter Component
    .

--- src/App.js
+++ src/App.js
@@ -8,24 +8,13 @@ import 'App.css';
 
 
 class App extends React.Component {
-  state = { myquery: "", language: "" }
-
-  onChange = (e) => {
-    const { name, value } = e.target;
-    console.log("changed value : ", value, this);
-    this.setState({
-      [name]: value,
-    });
-  }
-
   render() {
     return (
-      <>
-        <Counter onClick={() => { console.log("clicked") }} />
-        <input name="myquery" onChange={this.onChange} />
-        <input name="language" onChange={this.onChange} />
-        {JSON.stringify(this.state)}
-      </>
+      <div>
+        <Counter />
+        <Counter color="green" />
+        <Counter color="blue" />
+      </div>
     );
   }
 }
--- src/Counter.js
+++ src/Counter.js
@@ -3,23 +3,53 @@ import PropTypes from "prop-types";
 
 {/* .isRequired가 없으므로 옵션으로 인식 */ }
 class Counter extends React.Component {
-    static propTypes = {
-        onClick: PropTypes.func,
-
+    static defaultProps = {
+        color: 'red',
     }
 
+    static propTypes = {
+        color: PropTypes.string,
+    }
 
     state = {
+        color: this.props.color,
+        value: 0,
+    }
+    onClick = () => {
+        //this.setState({ value: this.state.value + 1 });
+        this.setState(({ value: preValue }) => ({
+            value: preValue + 1
+        }))
+    }
 
+    onContextMenu = (e) => {
+        e.preventDefault();
+        this.setState(({ value: preValue }) => ({
+            value: (preValue >= 1 ? preValue - 1 : 0)
+        }))
     }
 
     render() {
+        const { color, value } = this.state;
         return (
-            <div onClick={this.props.onClick}>
-                Counter
+            <div onClick={this.onClick}
+                onContextMenu={this.onContextMenu}
+                style={{ ...style, backgroundColor: color }}>
+                {value}
             </div>
         );
     }
+}
+
+const style = {
+    width: '100px',
+    height: '100px',
+    display: 'inline-block',
+    borderRadius: '50px',
+    textAlign: 'center',
+    lineHeight: '100px',
+    userSelect: 'none',
+    margin: '1rem',
 }
 
 export default Counter;
