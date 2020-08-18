69ea7328dbf7433e7c7c04b8e855cf740f49b740
Youngchan <michael_cho77@naver.com>
2020-08-18 12:19:46 +0900

    css, scss
    yarn add --dev node-sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Counter from "Counter";
+import Message from "Message";
+import Profile from "Profile";
 import 'App.css';
 
 
@@ -11,9 +13,12 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Profile />
+        <Message />
         <Counter />
         <Counter color="green" />
         <Counter color="blue" />
+
       </div>
     );
   }
---
+++ src/Message.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "Message.module.scss";
+
+class Message extends React.Component {
+    render() {
+        return (
+            <div className={styles.wrapper}>
+                Message Component
+            </div>
+        )
+    }
+}
+
+export default Message;
