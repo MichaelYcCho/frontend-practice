8a2ba0dfdcdd4ded36b8a8329f0bd7d24ea899ec
Youngchan <michael_cho77@naver.com>
2020-08-20 23:23:03 +0900

    ContextAPI
    .

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import App3 from './App3';
 import App4 from './App4';
 import App5 from './App5';
 import App6 from './App6';
+import App7 from './App7';
+import App8 from './App8';
 import TodoList from 'TodoList';
 import EpisodeList from "EpisodeList";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App6 />
+    <App8 />
   </React.StrictMode>,
   document.getElementById('root')
 );
