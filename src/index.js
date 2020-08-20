42e0cca83884372ad58adcd610d14a429a6d2474
Youngchan <michael_cho77@naver.com>
2020-08-21 02:29:48 +0900

    Router
    yarn add react-router-dom
    yarn add query-string

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ import App6 from './App6';
 import App7 from './App7';
 import App8 from './App8';
 import App9 from './App9';
+import App10 from './App10';
 import TodoList from 'TodoList';
 import EpisodeList from "EpisodeList";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App9 />
+    <App10 />
   </React.StrictMode>,
   document.getElementById('root')
 );
