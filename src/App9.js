8f52548472fbeb0d67394eaafef13777cc1e49f3
Youngchan <michael_cho77@naver.com>
2020-08-21 00:36:16 +0900

    ContextAPI Reducer Recap
    .

---
+++ src/App9.js
@@ -0,0 +1,48 @@
+import React, { useState, createContext, useReducer, useContext } from 'react';
+
+const CounterContext = createContext();
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+const reducer = (prevState, action) => {
+    //payload 값이 있으면 그 값 없으면 1이라는 뜻
+    const { type, payload: value = 1 } = action;
+    if (type === INCREMENT) {
+        return prevState + value;
+    } else if (type === DECREMENT) {
+        return prevState - value;
+    }
+    return prevState;
+};
+
+const actionIncrement = (value) => ({ type: INCREMENT, payload: value });
+const actionDecrement = (value) => ({ type: DECREMENT, payload: value });
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, 0);
+
+    return (
+        <div>
+            <h1 onClick={() => dispatch(actionDecrement(10))}>App9 : {state}</h1>
+            <CounterContext.Provider value={{ state, dispatch }}>
+                <GameBox />
+            </CounterContext.Provider>
+        </div>
+    );
+};
+
+const GameBox = () => {
+    const { state: countValue, dispatch } = useContext(CounterContext);
+    const onClick = () => {
+        dispatch(actionIncrement(5));
+    };
+    return (
+        <div>
+            <h2>GameBox</h2>
+            <button onClick={onClick}>{countValue}</button>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import App5 from './App5';
 import App6 from './App6';
 import App7 from './App7';
 import App8 from './App8';
+import App9 from './App9';
 import TodoList from 'TodoList';
 import EpisodeList from "EpisodeList";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App8 />
+    <App9 />
   </React.StrictMode>,
   document.getElementById('root')
 );
