7f68bc68ef1e926df69e0e029030f50e94722c1e
Youngchan <michael_cho77@naver.com>
2020-11-07 20:31:47 +0900

    PropTypes

--- src/HowToReact/Test.js
+++ src/HowToReact/Test.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 
-const Test = ({ food }) => {
-    return <div> 제가 좋아하는 음식은 {food} 입니다. </div>
+const Test = ({ name, food }) => {
+    return <div> 저는 {name} 입니다. <br />
+     제가 좋아하는 음식은 {food} 입니다. </div>
 }
 
 Test.defaultProps = {
-    food: '비빔밥'
+    food: '비빔밥',
+    name: "김민수"
+}
+
+Test.propTypes = {
+    food: PropTypes.string,
+    name: PropTypes.string.isRequired
 }
 
 export default Test;
