136ae0ebc617cbcbcd30a81eb1557be8bb7286ec
Youngchan <michael_cho77@naver.com>
2020-11-21 23:59:48 +0900

    Blog - State(class)

--- src/HowToReact/Test.js
+++ src/HowToReact/Test.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes, { number } from 'prop-types';
 
 
 
-const Test = ({ name, food }) => {
-    return <div> 저는 {name} 입니다. <br />
-     제가 좋아하는 음식은 {food} 입니다. </div>
+
+class Test extends Component {
+    state = {
+        number: 0
+    }
+
+    add = () => {
+        this.setState({ number: this.state.number + 1 })
+    }
+
+    minus = () => {
+        this.setState({ number: this.state.number - 1 })
+    }
+
+    render() {
+        const { number } = this.state;
+
+        return (
+            <div>
+                <h2> number is : {number}</h2>
+                <button onClick={this.add}> Add   </button>
+                <button onClick={this.minus}> Minus </button>
+            </div>
+        )
+
+    }
 }
 
+
+
+export default Test;
+
+
+
+
+
+/*
 Test.defaultProps = {
     food: '비빔밥',
     name: "김민수"
@@ -17,6 +49,4 @@ Test.propTypes = {
     food: PropTypes.string,
     name: PropTypes.string.isRequired
 }
-
-export default Test;
-
+*/
