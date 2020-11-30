3f8e54cbddda5658ffb4f2d9d08e98e66fded79f
Youngchan <michael_cho77@naver.com>
2020-12-01 00:27:18 +0900

    Get My Own Nweets

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,6 +5,7 @@ import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
 
+//App.js 에서 매개인자 날아왔음
 const AppRouter = ({ isLoggedIn, userObj }) => {
     return (
         <Router>
@@ -16,7 +17,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
                             <Home userObj={userObj} />
                         </Route>
                         <Route exact path="/profile">
-                            <Profile />
+                            <Profile userObj={userObj} />
                         </Route>
                     </>
                 ) : (
