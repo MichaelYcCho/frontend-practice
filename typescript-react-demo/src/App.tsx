e7e420d9825ad6d7540a0aad34150007ac4fd1c7
Youngchan <michael_cho77@naver.com>
2020-12-08 22:54:45 +0900

    Aggregation Repository(TypeScript)

---
+++ typescript-react-demo/src/App.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { createGlobalStyle } from "styled-components";
+import { Form, Input } from "./Input";
+import Number from "./Number";
+
+
+interface IState{
+  counter:number;
+  name:string;
+}
+
+
+class App extends Component<{}, IState> {
+  state = {
+    counter:0,
+    name:""
+  };
+  render() {
+    const {counter, name} = this.state;
+    return  (
+    <div> 
+       <Form onFormSubmit={this.onFormSubmit}>
+        <Input value={name} onChange={this.onChange} />
+      </Form>
+     <Number count={counter} /> <button onClick={this.add} >Add</button> 
+    </div>
+    );
+  }
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+
+  add = () => {
+    this.setState(prev => {
+      return {
+        counter:prev.counter + 1
+      }
+    })
+  }
+}
+
+export default App;
---
+++ typescript-react-demo/src/Input.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+interface IInpputProps {
+    value:string;
+    onChange: (event:React.SyntheticEvent<HTMLInputElement>) => void;
+}
+
+export const Input:React.FunctionComponent<IInpputProps> = ({value, onChange}) => (
+    <input type="text" placeholder="Name" value={value} onChange={onChange}/>
+)
+
+
+
+interface IFormProps {
+    onFormSubmit: (event: React.FormEvent) => void;
+  }
+  
+  export const Form: React.FunctionComponent<IFormProps> = ({
+    children,
+    onFormSubmit
+  }) => <form onSubmit={onFormSubmit}> {children}</form>;
+
