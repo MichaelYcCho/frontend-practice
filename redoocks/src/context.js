4b961692a4239cdeddba03dd7249e99d0eeb5345
Youngchan <michael_cho77@naver.com>
2020-12-11 22:58:23 +0900

    Update context.js

--- redoocks/src/context.js
+++ redoocks/src/context.js
@@ -1,27 +1,28 @@
 import React, { useState, useContext } from "react";
 
-export const UserContext = React.createContext()
+const UserContext = React.createContext();
 
 const UserContextProvider = ({ children }) => {
     const [user, setUser] = useState({
-        name: "Youngchan",
+        name: "YoungChan",
         loggedIn: false
     });
-    const logUserIn = () => setUser({ ...user, loggedIn: true })
+    const logUserIn = () => setUser({ ...user, loggedIn: true });
     return (
         <UserContext.Provider value={{ user, fn: { logUserIn } }}>
             {children}
         </UserContext.Provider>
-    )
-}
+    );
+};
 
 export const useUser = () => {
     const { user } = useContext(UserContext);
     return user;
-}
+};
 
 export const useFns = () => {
-    const { fn } = useContext(UserContext)
-}
+    const { fn } = useContext(UserContext);
+    return fn;
+};
 
 export default UserContextProvider;
