f4d643e4dad690c51d4ae6cf7bfa989484eadde9
Youngchan <michael_cho77@naver.com>
2020-12-12 23:09:33 +0900

    createRef()

--- react-practice/src/HowToReact/Test.js
+++ react-practice/src/HowToReact/Test.js
@@ -1,92 +1,23 @@
-import React, { useState } from 'react';
+import React, { Component } from "react";
 
+class Test extends Component {
+    input = React.createRef();
 
-const Test = () => {
-    const [answer, setAnswer] = useState();
+    inputClick = () => {
+        this.input.current.focus();
+    };
 
-    const onChange = (e) => {
-        setAnswer(e.target.value)
+    render() {
+        return (
+            <>
+                <input ref={this.input} />
+                <button onClick={this.inputClick}>Focus Input</button>
 
+            </>
+        );
     }
-
-    return (
-        <>
-            <input type="text" placeholder="Write Something" onChange={onChange} />
-            <div id="answer">{answer}</div>
-        </>
-    )
 }
-
-
 
 export default Test;
 
 
-
-/*
-const Test = () => {
-    const [number, setNumber] = useState(0);
-
-    const add = () => {
-        setNumber(prevNumber => prevNumber + 1);
-    }
-
-    const minus = () => {
-        setNumber(prevNumber => prevNumber - 1);
-    }
-
-    return (
-        <div>
-            <h2> number is : {number}</h2>
-            <button onClick={add}> Add   </button>
-            <button onClick={minus}> Minus </button>
-        </div>
-    );
-}
-
-class Test extends Component {
-    state = {
-        number: 0
-    }
-
-    add = () => {
-        this.setState({ number: this.state.number + 1 })
-    }
-
-    minus = () => {
-        this.setState({ number: this.state.number - 1 })
-    }
-
-    render() {
-        const { number } = this.state;
-
-        return (
-            <div>
-                <h2> number is : {number}</h2>
-                <button onClick={this.add}> Add   </button>
-                <button onClick={this.minus}> Minus </button>
-            </div>
-        )
-
-    }
-}
-
-*/
-
-
-
-
-
-/*
-import PropTypes, { number } from 'prop-types';
-
-Test.defaultProps = {
-    food: '비빔밥',
-    name: "김민수"
-}
-
-Test.propTypes = {
-    food: PropTypes.string,
-    name: PropTypes.string.isRequired
-}
-*/
