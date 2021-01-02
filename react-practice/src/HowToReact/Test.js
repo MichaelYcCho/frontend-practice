d5fe7b9ee701c91024862d883983404fdce08227
Youngchan <michael_cho77@naver.com>
2021-01-02 23:59:24 +0900

    Key

--- react-practice/src/HowToReact/Test.js
+++ react-practice/src/HowToReact/Test.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Test extends Component {
     render() {
         const brand = ['Samsung', 'Apple', 'LG', 'SK']
-        const brandList = brand.map(name => <p>{name}</p>);
-        return( 
+        const brandList = brand.map((name, index) => <p key={index}> {name}</p>);
+        return (
             <div>
                 {brandList}
             </div>
