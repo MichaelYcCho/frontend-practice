96c34ed92bed6c7beeab338e97dd080efafc4a19
Youngchan <michael_cho77@naver.com>
2021-01-23 22:25:52 +0900

    Content Base Logic

--- react-practice/src/HowToReact/Test.js
+++ react-practice/src/HowToReact/Test.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React, {useState} from 'react';
 
-class Test extends Component {
-    render() {
-        const brand = ['Samsung', 'Apple', 'LG', 'SK']
-        const brandList = brand.map((name, index) => <p key={index}> {name}</p>);
-        return (
-            <div>
-                {brandList}
-            </div>
-        );
-    }
+const Test = () => {
+    const [contents, setContents] = useState([
+        { id: 1, text: '1번 게시물'},
+        { id: 2, text: '2번 게시물'},
+        { id: 3, text: '3번 게시물'}
+    ]);
+    
+    const [text, setText] = useState('');
+    const [nextId, setNextId] = useState(4); 
+    
+    const contentList = contents.map(content => <li key={content.id}>{content.text}</li>);
+    return <ul>{contentList}</ul>
 }
 
+
+
+
 export default Test;
-
-
-
-
-
