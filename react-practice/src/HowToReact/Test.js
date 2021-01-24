d18569f6ad298f9ecfa676e77c64a4650ba6bafe
Youngchan <michael_cho77@naver.com>
2021-01-24 20:32:31 +0900

    insert DateValue with Array & State

--- react-practice/src/HowToReact/Test.js
+++ react-practice/src/HowToReact/Test.js
@@ -1,20 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Test = () => {
     const [contents, setContents] = useState([
-        { id: 1, text: '1번 게시물'},
-        { id: 2, text: '2번 게시물'},
-        { id: 3, text: '3번 게시물'}
+        { id: 1, text: '1번 게시물' },
+        { id: 2, text: '2번 게시물' },
+        { id: 3, text: '3번 게시물' }
     ]);
-    
+
     const [text, setText] = useState('');
-    const [nextId, setNextId] = useState(4); 
-    
+    const [nextId, setNextId] = useState(contents.length + 1);
+
+
+    const onChange = e => setText(e.target.value);
+    const onClick = () => {
+        const newContent = contents.concat({
+            id: nextId,
+            text
+        });
+        setNextId(nextId + 1)
+        setContents(newContent);
+        setText('');
+    }
+
     const contentList = contents.map(content => <li key={content.id}>{content.text}</li>);
-    return <ul>{contentList}</ul>
+
+    return (
+        <>
+            <input value={text} onChange={onChange} />
+            <button onClick={onClick}>Add</button>
+            <ul>{contentList}</ul>
+        </>
+    )
 }
-
-
-
 
 export default Test;
