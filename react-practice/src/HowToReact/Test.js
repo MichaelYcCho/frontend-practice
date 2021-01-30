f42913976288ccd80fbf75b56f79c08de84e7369
Youngchan <michael_cho77@naver.com>
2021-01-30 23:23:23 +0900

    useEffect

--- react-practice/src/HowToReact/Test.js
+++ react-practice/src/HowToReact/Test.js
@@ -1,34 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Test = () => {
-    const [contents, setContents] = useState([
-        { id: 1, text: '1번 게시물' },
-        { id: 2, text: '2번 게시물' },
-        { id: 3, text: '3번 게시물' }
-    ]);
 
-    const [text, setText] = useState('');
-    const [nextId, setNextId] = useState(contents.length + 1);
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
 
+    useEffect(() => {
+        console.log("useEffect 실행")
+        console.log(`Effect 실행 후 가진 name값은 ${name}`)
+        return () => {
+            console.log("정리를 합시다 ")
+            console.error(`정리하기전에 가진 name값은 ${name}`)
+        }
+    })
 
-    const onChange = e => setText(e.target.value);
-    const onClick = () => {
-        const newContent = contents.concat({
-            id: nextId,
-            text
-        });
-        setNextId(nextId + 1)
-        setContents(newContent);
-        setText('');
+    const onChangeName = event => {
+        setName(event.target.value);
     }
 
-    const contentList = contents.map(content => <li key={content.id}>{content.text}</li>);
+    const onChangeNickname = event => {
+        setNickname(event.target.value);
+    }
 
     return (
         <>
-            <input value={text} onChange={onChange} />
-            <button onClick={onClick}>Add</button>
-            <ul>{contentList}</ul>
+            <p>
+                <input value={name} onChange={onChangeName} placeholder="name" />
+                현재 이름: {name}
+            </p>
+            <p>
+                <input value={nickname} onChange={onChangeNickname} placeholder="nickname" />
+                현재 닉네임: {nickname}
+            </p>
         </>
     )
 }
