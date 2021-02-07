323649b93a395538e2c53772c79c5a3745af4694
Youngchan <michael_cho77@naver.com>
2021-02-07 11:39:24 +0900

    (MovieQL) - Rest with GraphQL

--- GraphQL/movieQL/graphql/db.js
+++ GraphQL/movieQL/graphql/db.js
@@ -1,51 +1,11 @@
-let movies = [
-    {
-        id: 0,
-        name: "Star Wars - The new one",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Avengers - The new one",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "The Godfather I",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Logan",
-        score: 2
-    }
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json"
 
 
-// 해당하는 영화들을 return
-export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id)
-    return filteredPeople[0]
-}
+export const getMovies = (limit, rating) => {
+    return fetch(`${API_URL}`)
+        .then(res => res.json())
+        .then(json => json.data.movies);
 
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies
-        return true;
-    } else {
-        return false;
-    }
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length + 1,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
+} 
