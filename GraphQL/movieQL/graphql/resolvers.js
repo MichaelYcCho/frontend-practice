4fc1c029cb9f35fd2931a8d0ec1d2e20d45435d8
Youngchan <michael_cho77@naver.com>
2021-02-07 14:48:56 +0900

    (MovieQL) - Rest with GraphQL Part3

--- GraphQL/movieQL/graphql/resolvers.js
+++ GraphQL/movieQL/graphql/resolvers.js
@@ -1,12 +1,11 @@
-import { getMovies } from "./db"
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
     Query: {
-        movies: (_, { rating, limit }) => getMovies(limit, rating)
-
+        movies: (_, { rating, limit }) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id),
+        suggestions: (_, { id }) => getSuggestions(id)
     }
-}
+};
 
-export default resolvers
-
-
+export default resolvers;
