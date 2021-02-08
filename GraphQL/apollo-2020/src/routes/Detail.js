cc88505d7330748d5e50f516bf4f7efd1a77fdd7
Youngchan <michael_cho77@naver.com>
2021-02-09 00:10:09 +0900

    (Apollo) - GET MOVIE Query

--- GraphQL/apollo-2020/src/routes/Detail.js
+++ GraphQL/apollo-2020/src/routes/Detail.js
@@ -1,1 +1,28 @@
-export default () => "detail"
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client"
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: { id: +id },
+        // 또는 variables: { id: Number(id) }
+    });
+    if (loading) {
+        return "loading";
+    }
+    if (data && data.movie) {
+        return data.movie.title;
+    }
+};
--- GraphQL/apollo-2020/src/routes/Home.js
+++ GraphQL/apollo-2020/src/routes/Home.js
@@ -1,3 +1,65 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client"
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+{
+    movies{
+        id
+        medium_cover_image
+    }
+}
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+export default () => {
+    const { loading, data } = useQuery(GET_MOVIES);
+    return (
+        <Container>
+            <Header>
+                <Title>Apollo 2020</Title>
+                <Subtitle>I love GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {!loading &&
+                data.movies &&
+                data.movies.map(movie => <Movie key={movie.id} id={movie.id} />)}
+        </Container>
+    );
+};
+
