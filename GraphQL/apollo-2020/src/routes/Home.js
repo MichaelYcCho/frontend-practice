5cae4917a2a4ec287ad594255218a6b9c693fe5b
Youngchan <michael_cho77@naver.com>
2021-02-10 16:08:57 +0900

    (Apollo) - Data & Apollo Dev Tools

--- GraphQL/apollo-2020/src/routes/Home.js
+++ GraphQL/apollo-2020/src/routes/Home.js
@@ -47,6 +47,17 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+
 export default () => {
     const { loading, data } = useQuery(GET_MOVIES);
     return (
@@ -56,9 +67,13 @@ export default () => {
                 <Subtitle>I love GraphQL</Subtitle>
             </Header>
             {loading && <Loading>Loading...</Loading>}
-            {!loading &&
-                data.movies &&
-                data.movies.map(movie => <Movie key={movie.id} id={movie.id} />)}
+            {!loading && data.movies && (
+              <Movies>
+                {data.movies.map(movie => (
+                  <Movie key={movie.id} id={movie.id} bg={movie.medium_cover_image} />
+                ))}
+              </Movies>
+            )}
         </Container>
     );
 };
