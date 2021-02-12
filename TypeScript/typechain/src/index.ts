80c5842681077c6797955667cc95fdfcf1a3b67e
Youngchan <michael_cho77@naver.com>
2021-02-13 01:05:11 +0900

    (TypeChain) - Interfaces on Typescript

--- TypeScript/typechain/src/index.ts
+++ TypeScript/typechain/src/index.ts
@@ -1,8 +1,22 @@
-const sayHi = (name:string, age:number, gender: string): string => {
-    return (`Hello ${name}, you are ${age}, you are a ${gender}`)
-}
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+  }
 
-sayHi("michael", 293, "male")
+const person = {
+    name: "michael",
+    age: 283,
+    gender: "male"
+  };
+
+  const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${
+      person.gender
+    }!`;
+};
+
+console.log(sayHi(person))
 
 export {}
 
