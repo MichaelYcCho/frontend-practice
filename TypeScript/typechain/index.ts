f2228a4c645385f0920925530d36ec0e070c202f
Youngchan <michael_cho77@naver.com>
2021-02-12 22:45:47 +0900

    (TypeChain) - First steps with Typescript

---
+++ TypeScript/typechain/index.ts
@@ -0,0 +1,13 @@
+const name = "michael",
+    age = 24,
+    gender = "male";
+
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name, age)
+
+export {}
+
