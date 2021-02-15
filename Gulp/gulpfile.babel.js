7b2b0cb615bad7609150a84f3714a6a2bb2ec0e6
Youngchan <michael_cho77@naver.com>
2021-02-15 23:11:40 +0900

    (Gulp) - Sass gulp(1)

--- Gulp/gulpfile.babel.js
+++ Gulp/gulpfile.babel.js
@@ -3,6 +3,9 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
 
 const routes = {
     pug: {
@@ -13,6 +16,11 @@ const routes = {
     img: {
         src: "src/img/*",
         dest: "build/img"
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css"
     }
 };
 
@@ -33,14 +41,22 @@ const img = () =>
         .pipe(image())
         .pipe(gulp.dest(routes.img.dest));
 
+
+const styles = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(routes.scss.dest));
+
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.img.src, img);
+    gulp.watch(routes.scss.watch, styles);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const live = gulp.parallel([webserver, watch]);
 
