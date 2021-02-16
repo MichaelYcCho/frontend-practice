f5aa4d6c02c7c8e1862667cef9054fe1b4c1e486
Youngchan <michael_cho77@naver.com>
2021-02-17 00:45:45 +0900

    (NestAPI) - Movies Service part Two

--- Nest/nest-api/src/movies/movies.controller.ts
+++ Nest/nest-api/src/movies/movies.controller.ts
@@ -4,34 +4,31 @@ import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
-  constructor(private movieService: MoviesService){}
+  constructor(private moviesService: MoviesService){}
 
   @Get()
   getAll() : Movie[] {
-    return this.movieService.getAll();
+    return this.moviesService.getAll();
   }
 
   @Get(':id')
   getOne(@Param('id') movieId: string): Movie {
-    return this.movieService.getOne(movieId);
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return this.movieService.create(movieData);
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return this.movieService.deleteOne(movieId);
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData) {
-    return{
-      updatedMovie: movieId,
-      ...updateData
-    }
+    return this.moviesService.update(movieId, updateData);
   }
 
 
