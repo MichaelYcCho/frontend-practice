6fbfecc1cc5e69a8c78783cc0bea23f8a5da05be
Youngchan <michael_cho77@naver.com>
2021-02-19 14:42:00 +0900

    (NestAPI) - Testing getAll and getOne

--- Nest/nest-api/src/movies/movies.service.spec.ts
+++ Nest/nest-api/src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { isRgbColor } from 'class-validator';
 import { MoviesService } from './movies.service';
@@ -17,9 +18,32 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
+  describe("getAll", () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
 
-  it('should be 4', () => {
-    expect(2+2).toEqual(4)
-  })
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
 
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.')
+      }
+    });
+
+  });
 });
