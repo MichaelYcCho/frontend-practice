047d4fc523fbb5ee1b9f48d772f4a0230e4dffb9
Youngchan <michael_cho77@naver.com>
2021-02-21 23:35:51 +0900

    (redoocks) - useContext in Action

--- React/redoocks/src/Header.js
+++ React/redoocks/src/Header.js
@@ -2,13 +2,11 @@ import React, { useContext } from "react";
 import { UserContext } from "./context";
 
 const Header = () => {
-    const {
-        user: { name, loggedIn }
-    } = useContext(UserContext);
+    const { user: { name, loggedIn } } = useContext(UserContext);
     return (
         <header>
-            <a href="#">Home</a> Hello, {name}, you are{" "}
-            {loggedIn ? "logged in" : "anonymous"}
+            <a href="#">Home</a> Hello, {name}, you are {" "}
+            {loggedIn ? "logged in " : "anonymous"}
         </header>
     );
 };
--- React/redoocks/src/Screen.js
+++ React/redoocks/src/Screen.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./Header";
+import { UserContext } from "./context";
 
 const Screen = () => {
-    return 'screen'
+    const { logUserIn } = useContext(UserContext);
+    return (
+        <div>
+            <Header />
+            <h1> Screen </h1>
+            <button onClick={logUserIn}>Log user in</button>
+        </div>
+    );
 };
 export default Screen;
