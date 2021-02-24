9aa28b98363e8f6c1d485c03f931685565cd2eb3
Youngchan <michael_cho77@naver.com>
2021-02-25 01:00:44 +0900

    (Redux) - Delete Todos

--- React/redux-todo/src/App.js
+++ React/redux-todo/src/App.js
@@ -1,18 +1,22 @@
-
 import React, { useState, useReducer } from "react";
+import { v4 as uuid } from "uuid";
 
 
 const initialState = {
     toDos: []
-}
+};
 
-const ADD = "increment";
-
+const ADD = "add";
+const DEL = "del";
 
 const reducer = (state, action) => {
     switch (action.type) {
         case ADD:
-            return { toDos: [...state.toDos, { text: action.payload }] };
+            return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
+        case DEL:
+            return {
+                toDos: state.toDos.filter(toDo => toDo.id !== action.payload)
+            };
         default:
             return;
     }
@@ -23,25 +27,34 @@ function App() {
     const [newToDo, setNewToDo] = useState("");
     const onSubmit = e => {
         e.preventDefault();
-        dispatch({ type: ADD, payload: newToDo })
+        dispatch({ type: ADD, payload: newToDo });
+        setNewToDo("");
     };
     const onChange = e => {
         const {
             target: { value }
         } = e;
-        setNewToDo(value)
-    }
+        setNewToDo(value);
+    };
     return (
         <>
-            <h1>To Dos</h1>
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Write to do" onChange={onChange} />
+                <input
+                    value={newToDo}
+                    type="text"
+                    placeholder="Write to do"
+                    onChange={onChange}
+                />
             </form>
-
             <ul>
                 <h2>To Dos</h2>
-                {state.toDos.map((toDo, index) => (
-                    <li key={index}>{toDo.text}</li>
+                {state.toDos.map(toDo => (
+                    <li key={toDo.id}>
+                        <span>{toDo.text}</span>
+                        <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                            ❌
+              </button>
+                    </li>
                 ))}
             </ul>
         </>
