1fe6b79e82964275b2b745f686766efff717471c
Youngchan <michael_cho77@naver.com>
2021-02-25 22:45:09 +0900

    (Redux) - Uncompleting To Dos

--- React/redux-todo/src/reducer.js
+++ React/redux-todo/src/reducer.js
@@ -13,17 +13,29 @@ export const UNCOMPLETE = "uncomplete";
 const reducer = (state, action) => {
     switch (action.type) {
         case ADD:
-            return { ...state, toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
+            return {
+                ...state,
+                toDos: [...state.toDos, { text: action.payload, id: uuid() }]
+            };
         case DEL:
             return {
-                ...state, toDos: state.toDos.filter(toDo => toDo.id !== action.payload)
+                ...state,
+                toDos: state.toDos.filter(toDo => toDo.id !== action.payload)
             };
         case COMPLETE:
-            const target = state.toDos.find(toDo => toDo.id === action.payload)
+            const target = state.toDos.find(toDo => toDo.id === action.payload);
             return {
-                ...state, toDos: state.toDos.filter(toDo => toDo.id !== action.payload),
+                ...state,
+                toDos: state.toDos.filter(toDo => toDo.id !== action.payload),
                 completed: [...state.completed, { ...target }]
-            }
+            };
+        case UNCOMPLETE:
+            const aTarget = state.completed.find(toDo => toDo.id === action.payload);
+            return {
+                ...state,
+                completed: state.completed.filter(toDo => toDo.id !== action.payload),
+                toDos: [...state.toDos, { ...aTarget }]
+            };
         default:
             return;
     }
