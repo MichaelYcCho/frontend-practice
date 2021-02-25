fd4b056f8075e6425fe7c839f9cf5c8dbf50a368
Youngchan <michael_cho77@naver.com>
2021-02-25 22:41:15 +0900

    (Redux) - Completing To Dos

--- React/redux-todo/src/App.js
+++ React/redux-todo/src/App.js
@@ -1,26 +1,5 @@
 import React, { useState, useReducer } from "react";
-import { v4 as uuid } from "uuid";
-
-
-const initialState = {
-    toDos: []
-};
-
-const ADD = "add";
-const DEL = "del";
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case ADD:
-            return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
-        case DEL:
-            return {
-                toDos: state.toDos.filter(toDo => toDo.id !== action.payload)
-            };
-        default:
-            return;
-    }
-};
+import reducer, { initialState, ADD, DEL, COMPLETE, UNCOMPLETE } from "./reducer";
 
 function App() {
     const [state, dispatch] = useReducer(reducer, initialState);
@@ -51,11 +30,36 @@ function App() {
                 {state.toDos.map(toDo => (
                     <li key={toDo.id}>
                         <span>{toDo.text}</span>
-                        <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                        <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
                             ❌
-              </button>
+                        </span>
+                        <span onClick={() => dispatch({ type: COMPLETE, payload: toDo.id })}>
+                            ✅
+                        </span>
                     </li>
                 ))}
+            </ul>
+            <ul>
+                {state.completed.length !== 0 && (
+                    <>
+                        <h2>Completed</h2>
+                        {state.completed.map(toDo => (
+                            <li key={toDo.id}>
+                                <span>{toDo.text}</span>
+                                <span role="img" aria-label=""
+                                    onClick={() => dispatch({ type: DEL, payload: toDo.id })}
+                                >
+                                    ❌
+                                </span>
+                                <span role="img" aria-label=""
+                                    onClick={() => dispatch({ type: UNCOMPLETE, payload: toDo.id })}
+                                >
+                                    🙅🏼‍♂️
+                                </span>
+                            </li>
+                        ))}
+                    </>
+                )}
             </ul>
         </>
     );
