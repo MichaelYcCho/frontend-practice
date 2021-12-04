3482e4f4043e090116741020c9ae07cfd6a53e66
Youngchan <michael.yc.cho@gmail.com>
2021-12-04 14:40:20 +0900

    feature/Attr Tricks

---
+++ React/react-2021/src/App.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
+`;
+
+function App() {
+  return (
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
+  );
+}
+
+export default App;
+
